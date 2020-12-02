93e9ed535f53c0c21c504eab04eed8c19c3d7002
khigh <khigh@KeenansWhitePC>
2020-12-02 12:03:03 -0700

    updated the dialog bod for add att

---
+++ CSE360FinalProj/src/Attendance.java
@@ -0,0 +1,41 @@
+public class Attendance {
+    
+	//declare variables
+    private String asurite;
+    private String time;
+
+    //constructor
+    public Attendance(String asurite, String time){
+        this.asurite = asurite;
+        this.time = time;
+    }
+
+    //default constructor
+    public Attendance(){
+        asurite = "asurite";
+        time = "100";
+    }
+
+    //setters
+    public void setAsurite(String asurite) {
+        this.asurite = asurite;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAsurite(){
+        return asurite;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public int addTime(int newTime){
+            int timeInt = Integer.parseInt(time);
+    		timeInt += newTime;
+            return timeInt;
+            }
+}
